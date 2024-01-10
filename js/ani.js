087c0d2f57ff3dd26df5c64dac81ca29b24d45c6
nurbeki1 <127381013+nurbeki1@users.noreply.github.com>
2024-01-10 17:06:24 +0600

    Add files via upload

---
+++ js/ani.js
@@ -0,0 +1,30 @@
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".swiper");
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,58 @@
-const button_menu = document.querySelector(".burg_menu");
-const menu = document.querySelector("#menua");
-console.log(button_menu);
+const menu = document.querySelector("#menu");
+const btn = document.querySelector("#btn_burger");
+const btnImg = document.querySelector("#burgerImg");
 
-button_menu.addEventListener("click", function () {
-  menu.classList.toggle("mn_burg");
+menu.addEventListener("click", function () {
+  console.log("asdfa");
+
+  if (menu.classList.toggle("open")) {
+    btnImg.src = "photos/close.png";
+  } else {
+    btnImg.src = "photos/open.png";
+  }
 });
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    } else {
+      change.target.classList.remove("element-show");
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let element1 = document.querySelectorAll(".text_b2");
+let card1 = document.querySelectorAll(".card1");
+let card2 = document.querySelectorAll(".card2");
+let card3 = document.querySelectorAll(".card3");
+
+let card1_5 = document.querySelectorAll(".card_5_1");
+let card2_5 = document.querySelectorAll(".card_5_2");
+let card3_5 = document.querySelectorAll(".card_5_3");
+for (let elm of element1) {
+  observer.observe(elm);
+}
+for (let elm of card1) {
+  observer.observe(elm);
+}
+
+for (let elm of card2) {
+  observer.observe(elm);
+}
+for (let elm of card3) {
+  observer.observe(elm);
+}
+for (let elm of card1_5) {
+  observer.observe(elm);
+}
+for (let elm of card2_5) {
+  observer.observe(elm);
+}
+for (let elm of card3_5) {
+  observer.observe(elm);
+}
